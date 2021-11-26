957dcfa223bf329e33198ff5aa6fb2e36c4e3681
DESKTOP-BBB6KJQ\User <kightkuku@naver.com>
2021-11-27 00:47:16 +0900

    1. 변경사항 - (중고거래 -> 상품선택 -> 구매하기) 현재 총 3페이지 전환 소스와 백그라운드 파일 추가 - 백그라운드 파일 drawble 디렉토리에 추가함
    2. 유의사항
    - 피그마 2번 방법 페이지에 이미지를 화면에 맞게 변경하였음
    - 현재 버튼은 투명버튼이 아님. 우려한대로 백그라운드 이미지의 비율이 변하면 버튼의 위치와 호환되고 있지 않음

--- app/src/main/java/com/mintcho/wwmarket2/SubActivity.kt
+++ app/src/main/java/com/mintcho/wwmarket2/SubActivity.kt
@@ -5,13 +5,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class SubActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)   //layout 디렉토리의 xml와 1대1 매칭되도록 activity_main
+        setContentView(R.layout.activity_sub)
 
         mButton.setOnClickListener {
-            val intent = Intent(this, SubActivity::class.java)  //버튼 누르면 넘어갈 페이지를 Subactivity 위치에 알맞게 변경할것
+            val intent = Intent(this, SubActivity2::class.java)
             startActivity(intent)
         }
     }
---
+++ app/src/main/java/com/mintcho/wwmarket2/SubActivity2.kt
@@ -0,0 +1,11 @@
+package com.mintcho.wwmarket2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SubActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sub2)
+    }
+}
