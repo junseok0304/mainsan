7c321292f566a9a3ce3c52b89a14c47ae4df6b5d
junseok0304 <wicaduce@gmail.com>
2021-11-28 02:26:48 +0900

    리얼월드 추가

---
+++ app/src/main/java/com/mintcho/wwmarket2/MainActivity_realworld.kt
@@ -0,0 +1,26 @@
+package com.mintcho.wwmarket2
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main_realworld.*
+
+class MainActivity_realworld : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_realworld) //layout 디렉토리의 xml와 1대1 매칭되도록 activity_main
+
+        mButton.setOnClickListener {
+            val intent = Intent(this, SubActivity::class.java)  //버튼 누르면 넘어갈 페이지를 Subactivity 위치에 알맞게 변경할것
+            startActivity(intent)
+        }
+
+        realButton.setOnClickListener {
+            val intent = Intent(this, activity_market1::class.java)  //버튼 누르면 넘어갈 페이지를 Subactivity 위치에 알맞게 변경할것
+            startActivity(intent)
+        }
+
+
+
+    }
+}
