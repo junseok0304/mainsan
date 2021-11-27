6fabe10dfc3977f5bd0030ad860677779646225c
junseok0304 <wicaduce@gmail.com>
2021-11-27 18:08:48 +0900

    버튼 하나 추가, 배경 확대버젼으로 변경(파일은 그대로, code에서 불러오는 백그라운드 파일 명만 수정함)

--- app/src/main/java/com/mintcho/wwmarket2/MainActivity.kt
+++ app/src/main/java/com/mintcho/wwmarket2/MainActivity.kt
@@ -8,7 +8,8 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)   //layout 디렉토리의 xml와 1대1 매칭되도록 activity_main
+        setContentView(R.layout.activity_main) //layout 디렉토리의 xml와 1대1 매칭되도록 activity_main
+        supportActionBar?.hide()
 
         mButton.setOnClickListener {
             val intent = Intent(this, SubActivity::class.java)  //버튼 누르면 넘어갈 페이지를 Subactivity 위치에 알맞게 변경할것
