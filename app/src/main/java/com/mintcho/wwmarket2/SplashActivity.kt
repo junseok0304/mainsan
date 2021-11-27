5d88d04f0c6c97028159ab48a3031c93fef97f5d
junseok0304 <wicaduce@gmail.com>
2021-11-27 22:22:12 +0900

    스플래시 화면추가, 버튼연동

---
+++ app/src/main/java/com/mintcho/wwmarket2/SplashActivity.kt
@@ -0,0 +1,20 @@
+package com.mintcho.wwmarket2
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            // 끝
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 3000)
+    }
+}
