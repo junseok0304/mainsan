07940827569a77c21b15b46aa8e8a18e4ce4f4fb
junseok0304 <wicaduce@gmail.com>
2021-11-29 08:13:28 +0900

    다수수정

---
+++ app/src/main/java/com/mintcho/wwmarket2/realoffer2.kt
@@ -0,0 +1,11 @@
+package com.mintcho.wwmarket2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class realoffer2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_realoffer2)
+    }
+}
